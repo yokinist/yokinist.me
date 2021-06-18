540b5db281794c8a181fa4ff18b221d67166dfbc
yokinist <ikd618@gmail.com>
2021-06-19 04:49:51 +0900

    :jigsaw: add @types

---
+++ @types/index.ts
@@ -0,0 +1,20 @@
+/* eslint-disable camelcase */
+
+export type PostType = "Post" | "Page"
+
+export type PostStatus = "Idea" | "Published" | "Revise" | "Published"
+
+export type Post = {
+  id: string
+  createdTime: string
+  fullWidth: boolean
+  title?: string
+  slug?: string
+  summary?: string
+  tags?: string[]
+  date: {
+    start_date?: string
+  }
+  status?: [PostStatus]
+  type?: [PostStatus]
+}
