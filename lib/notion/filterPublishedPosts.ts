c3e4e0df939c680ce9476b2e054a64309fb3ef47
yokinist <ikd618@gmail.com>
2021-06-20 13:07:54 +0900

    :wrench: toLocaleDateString

--- lib/notion/filterPublishedPosts.ts
+++ lib/notion/filterPublishedPosts.ts
@@ -1,17 +1,20 @@
 import { Post } from '@/types'
+import BLOG from '@/blog.config'
 
-const currentDate = new Date()
+const currentDate = new Date().toLocaleDateString(BLOG.lang)
 
 export const filterPublishedPosts = (posts: Post[] | null) => {
   if (!posts || !posts.length) return []
   const publishedPosts = posts.filter(post => {
-    const postDate = new Date(post?.date?.start_date || post.createdTime)
+    const postDate = new Date(
+      post?.date?.start_date || post.createdTime
+    ).toLocaleDateString(BLOG.lang)
     return (
       post.title &&
       post.slug &&
       post?.status?.[0] === 'Published' &&
       (post?.type?.[0] === 'Post' || post?.type?.[0] === 'Page') &&
-      postDate.getTime() < currentDate.getTime()
+      postDate <= currentDate
     )
   })
   return publishedPosts
