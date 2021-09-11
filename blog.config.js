850961dbb29b23fced5e53cd69ace6125c7115e2
yokinist <ikd618@gmail.com>
2021-09-11 13:37:29 +0900

    update bg color

--- blog.config.js
+++ blog.config.js
@@ -11,7 +11,7 @@ const BLOG = {
   appearance: 'dark', // ['light', 'dark', 'auto'],
   font: 'sans-serif', // ['sans-serif', 'serif']
   lightBackground: '#ffffff', // use hex value, don't forget '#' e.g #fffefc
-  darkBackground: '#15202b', // use hex value, don't forget '#'
+  darkBackground: '#090719', // use hex value, don't forget '#'
   path: '', // leave this empty unless you want to deploy Nobelium in a folder
   since: 2021, // If leave this empty, current year will be used.
   postsPerPage: 7,
