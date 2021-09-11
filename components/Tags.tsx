f10e3ae004f942bc08b4d6dc934e69dc5ba390d3
yokinist <ikd618@gmail.com>
2021-09-11 20:12:03 +0900

    a11y: tag link

--- components/Tags.tsx
+++ components/Tags.tsx
@@ -11,31 +11,31 @@ const Tags: React.VFC<Props> = ({ tags, currentTag }: Props) => {
   if (!tags) return null
   return (
     <div className="tag-container">
-      <ul className="flex max-w-full mt-4 overflow-x-auto">
+      <ul className="flex max-w-full mt-4 overflow-x-auto h-full">
         {Object.keys(tags).map(key => {
           const selected = key === currentTag
           return (
-            <Link
+            <li
               key={key}
-              href={selected ? '/' : `/tag/${encodeURIComponent(key)}`}
-              scroll={false}
+              className={classNames(
+                'mr-3 font-medium border whitespace-nowrap dark:text-gray-300 h-8',
+                {
+                  'text-white bg-black border-black dark:bg-gray-600 dark:border-gray-600':
+                    selected,
+                  'bg-gray-100 border-gray-100 text-gray-400 dark:bg-night dark:border-gray-800':
+                    !selected
+                }
+              )}
             >
-              <a>
-                <li
-                  className={classNames(
-                    'mr-3 py-2 font-medium border px-4 whitespace-nowrap dark:text-gray-300',
-                    {
-                      'text-white bg-black border-black dark:bg-gray-600 dark:border-gray-600':
-                        selected,
-                      'bg-gray-100 border-gray-100 text-gray-400 dark:bg-night dark:border-gray-800':
-                        !selected
-                    }
-                  )}
-                >
+              <Link
+                href={selected ? '/' : `/tag/${encodeURIComponent(key)}`}
+                scroll={false}
+              >
+                <a className="px-2 align-text-top py-2">
                   {`${key} (${tags[key]})`}
-                </li>
-              </a>
-            </Link>
+                </a>
+              </Link>
+            </li>
           )
         })}
       </ul>
