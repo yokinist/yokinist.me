0f846a683f9a3d19bf15caac61db3e60e8d6bbf2
yokinist <ikd618@gmail.com>
2021-09-12 15:27:11 +0900

    update ogImageGenerateURL

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   showAbout: true,
   showArchive: true,
   autoCollapsedNavBar: false, // The automatically collapsed navigation bar
-  ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
+  ogImageGenerateURL: 'https://simple-og-image.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/yokinist',
   seo: {
     keywords: ['yokinist'],
--- components/Container.tsx
+++ components/Container.tsx
@@ -59,7 +59,9 @@ const Container: React.VFC<Props> = ({
           property="og:image"
           content={`${BLOG.ogImageGenerateURL}/${encodeURIComponent(
             meta.title
-          )}.png?theme=dark&md=1&fontSize=125px&images=https%3A%2F%2Fnobelium.vercel.app%2Flogo-for-dark-bg.svg`}
+          )}.png?md=1&fontSize=96px&siteTitle=${BLOG.title}&background=${
+            BLOG.darkBackground
+          }&foreground=${BLOG.lightBackground}&accentColor=#5a67d8`}
         />
         <meta property="og:type" content={meta.type} />
         <meta name="twitter:card" content="summary_large_image" />
