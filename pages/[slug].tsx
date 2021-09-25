0f6635e666f140b4083cbde766b1d5701b238035
yokinist <ikd618@gmail.com>
2021-09-25 13:00:00 +0900

    :recycle: refactor redirect to outer-link

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -5,14 +5,13 @@ import { getAllPosts, getPostBlocks } from '@/lib/notion'
 import BLOG from '@/blog.config'
 import { createHash } from 'crypto'
 import DefaultErrorPage from 'next/error'
-import { getIsClient } from '@/lib/getIsClient'
-
-const isClient = getIsClient()
 
 export const getStaticPaths: GetStaticPaths = async () => {
   const posts = await getAllPosts({ includedPages: true })
   if (!posts) return { paths: [], fallback: false }
-  const publishPosts = posts.filter(post => post?.status?.[0] === 'Published')
+  const publishPosts = posts.filter(
+    post => post?.status?.[0] === 'Published' && !post?.outer_link
+  )
   return {
     paths: publishPosts.map(row => `${BLOG.path}/${row.slug}`),
     fallback: true
@@ -24,6 +23,14 @@ export const getStaticProps: GetStaticProps = async context => {
   const posts = await getAllPosts({ includedPages: true })
   const post = posts.find(t => t.slug === slug)
   if (!post?.id) return { notFound: true }
+  if (post?.outer_link) {
+    return {
+      redirect: {
+        destination: post.outer_link,
+        permanent: true
+      }
+    }
+  }
   const blockMap = await getPostBlocks(post.id)
   const emailHash = createHash('md5').update(BLOG.email).digest('hex')
   return {
@@ -36,9 +43,6 @@ type Props = Omit<React.ComponentProps<typeof Layout>, 'fullWidth'>
 
 const BlogPost: NextPage<Props> = ({ post, blockMap, emailHash }) => {
   if (!post) return <DefaultErrorPage statusCode={404} />
-  if (post?.outer_link && isClient) {
-    window.location.href = post.outer_link
-  }
   return (
     <>
       <TwitterContextProvider
