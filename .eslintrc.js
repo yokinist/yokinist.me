140d72a9e255ca622be10618c61f07fac7ea38d8
yokinist <ikd618@gmail.com>
2021-10-19 22:29:38 +0900

    :wrench: ignore tailwindcss/no-custom-classname

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,5 +3,6 @@ module.exports = {
   extends: ['next', 'next/core-web-vitals', 'prettier', 'plugin:tailwindcss/recommended'],
   rules: {
     '@next/next/no-img-element': 'off',
+    'tailwindcss/no-custom-classname': 'off',
   },
 };
