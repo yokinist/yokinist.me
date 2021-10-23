2137081e9393ddb596d1dd88bd0f5594b9ffd07f
yokinist <ikd618@gmail.com>
2021-10-23 23:15:16 +0900

    :lipstick: tweak tag name styling

--- components/Tag/TagTabItem.tsx
+++ components/Tag/TagTabItem.tsx
@@ -21,7 +21,7 @@ export const TagTabItem: React.VFC<Props> = ({ tagKey, selected, ...rest }) => {
   return (
     <li
       className={classNames('mr-3 font-bold whitespace-nowrap rounded-lg', {
-        'text-gray-300  border-gray-100 dark:text-gray-300 dark:border-gray-700': !selected,
+        'text-gray-400 border-gray-100 dark:text-gray-300 dark:border-gray-700': !selected,
         'bg-gray-200 text-gray-700 dark:text-night': selected,
       })}
     >
