30139350905a3927a113a95fa655e0c6bd19c49e
yokinist <ikd618@gmail.com>
2021-10-26 15:56:01 +0900

    :recycle: Twemoji

--- components/Twemoji.tsx
+++ components/Twemoji.tsx
@@ -1,17 +1,19 @@
 import Image from 'next/image';
 import React from 'react';
 
+const BASE_TWEMOJI_CDN_URL = 'https://twemoji.maxcdn.com/v/latest/svg';
+
 type Props = {
   emoji: string;
   size?: number;
 };
 
 export const Twemoji: React.VFC<Props> = ({ emoji, size = 24 }) => {
-  const img = emoji.codePointAt(0)?.toString(16);
-  if (!img) return null;
+  const emojiString = emoji.codePointAt(0)?.toString(16);
+  if (!emojiString) return null;
   return (
     <span className="flex items-center min-w-max">
-      <Image src={`https://twemoji.maxcdn.com/v/latest/svg/${img}.svg`} height={size} width={size} alt={emoji} />
+      <Image src={`${BASE_TWEMOJI_CDN_URL}/${emojiString}.svg`} height={size} width={size} alt={emoji} />
     </span>
   );
 };
