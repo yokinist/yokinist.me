0b0802121b2469282bfb7a8fa63deab71fb87a6f
yokinist <ikd618@gmail.com>
2021-10-27 03:18:50 +0900

    :wrench: no-console

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,9 @@
-/** @type {import('@typescript-eslint/experimental-utils').TSESLint.Linter.Config} */
+/** @type {import('eslint-config-next')*/
 module.exports = {
   extends: ['next', 'next/core-web-vitals', 'prettier', 'plugin:tailwindcss/recommended'],
   rules: {
     '@next/next/no-img-element': 'off',
     'tailwindcss/no-custom-classname': 'off',
+    'no-console': ['error', { allow: ['warn', 'error'] }],
   },
 };
