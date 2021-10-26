e2ae3a135d9e1ff5d64fb38cd78c4b277534be3a
yokinist <ikd618@gmail.com>
2021-10-27 02:27:13 +0900

    :wrench: split chunks

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-
+const preact = require('preact');
 /**
  * @type {import('next').NextConfig}
  **/
@@ -27,14 +27,55 @@ const nextConfig = {
   },
   webpack: (config, { dev, isServer }) => {
     config.resolve.alias['~'] = path.join(__dirname, '.');
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: 'preact/compat',
-        'react-dom/test-utils': 'preact/test-utils',
-        'react-dom': 'preact/compat',
-      });
+    const splitChunks = config.optimization && config.optimization.splitChunks;
+    if (splitChunks) {
+      const cacheGroups = splitChunks.cacheGroups;
+      const preactModules = /[\\/]node_modules[\\/](preact|preact-render-to-string|preact-context-provider)[\\/]/;
+      if (cacheGroups.framework) {
+        cacheGroups.preact = Object.assign({}, cacheGroups.framework, {
+          test: preactModules,
+        });
+        cacheGroups.commons.name = 'framework';
+      } else {
+        cacheGroups.preact = {
+          name: 'commons',
+          chunks: 'all',
+          test: preactModules,
+        };
+      }
     }
+
+    // Install webpack aliases:
+    const aliases = config.resolve.alias || (config.resolve.alias = {});
+    aliases.react = aliases['react-dom'] = 'preact/compat';
+
+    if (dev) {
+      if (isServer) {
+        // Remove circular `__self` and `__source` props only meant for
+        // development. See https://github.com/developit/nextjs-preact-demo/issues/25
+        let oldVNodeHook = preact.options.vnode;
+        preact.options.vnode = (vnode) => {
+          const props = vnode.props;
+          if (props != null) {
+            if ('__self' in props) props.__self = null;
+            if ('__source' in props) props.__source = null;
+          }
+
+          if (oldVNodeHook) {
+            oldVNodeHook(vnode);
+          }
+        };
+      } else {
+        // inject Preact DevTools
+        const entry = config.entry;
+        config.entry = () =>
+          entry().then((entries) => {
+            entries['main.js'] = ['preact/debug'].concat(entries['main.js'] || []);
+            return entries;
+          });
+      }
+    }
+
     return config;
   },
 };
