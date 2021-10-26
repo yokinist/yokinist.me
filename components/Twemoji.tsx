f8294189df06a9822db27f4bb4a8263ff65e09f6
yokinist <ikd618@gmail.com>
2021-10-27 02:52:57 +0900

    :fire: remove duplicated span tag

--- components/Twemoji.tsx
+++ components/Twemoji.tsx
@@ -12,8 +12,12 @@ export const Twemoji: React.VFC<Props> = ({ emoji, size = 24 }) => {
   const emojiString = emoji.codePointAt(0)?.toString(16);
   if (!emojiString) return null;
   return (
-    <span className="flex items-center min-w-max">
-      <Image src={`${TWEMOJI_CDN_BASE_URL}/${emojiString}.svg`} height={size} width={size} alt={emoji} />
-    </span>
+    <Image
+      className="flex items-center min-w-max"
+      src={`${TWEMOJI_CDN_BASE_URL}/${emojiString}.svg`}
+      height={size}
+      width={size}
+      alt={emoji}
+    />
   );
 };
