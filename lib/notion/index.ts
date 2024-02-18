d57c5c1e647640b6c4e0165162ab6c3f86ed4253
yokinist <ikd618@gmail.com>
2024-02-18 22:20:45 +0900

    refactor: Remove unused getMetadata function

--- lib/notion/index.ts
+++ lib/notion/index.ts
@@ -2,7 +2,6 @@ export * from "./filterPublishedPosts";
 export * from "./getAllPageIds";
 export * from "./getAllPosts";
 export * from "./getAllTags";
-export * from "./getMetadata";
 export * from "./getPageProperties";
 export * from "./getPostBlocks";
 export * from "./getProfilePost";
