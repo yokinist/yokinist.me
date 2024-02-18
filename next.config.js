007c3275b4174bdc2307293d4c77f4eaf2af573b
yokinist <ikd618@gmail.com>
2024-02-18 17:10:28 +0900

    fix: remove preact for production build

--- next.config.js
+++ next.config.js
@@ -22,16 +22,8 @@ const nextConfig = {
       },
     ];
   },
-  webpack: (config, { dev, isServer }) => {
+  webpack: (config) => {
     config.resolve.alias["~"] = path.join(__dirname, ".");
-    // Replace React with Preact only in client production build
-    if (!dev && !isServer) {
-      Object.assign(config.resolve.alias, {
-        react: "preact/compat",
-        "react-dom/test-utils": "preact/test-utils",
-        "react-dom": "preact/compat",
-      });
-    }
     return config;
   },
 };
