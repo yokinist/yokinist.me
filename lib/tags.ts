c24b4a50f71dbc069bc9a150494db106339a649a
yokinist <ikd618@gmail.com>
2024-02-18 18:00:36 +0900

    fix: default tags

--- lib/tags.ts
+++ lib/tags.ts
@@ -1,24 +1,17 @@
 import { ValueOf } from "lib/types";
 
+// NOTE: you can customize tags to match the tags in your Notion database
 const TAG_SLUGS = {
   All: "all",
-  Tech: "tech",
-  Update: "update",
-  Music: "music",
-  Book: "book",
-  Playlist: "playlist",
-  CultureBook: "culture-book",
+  Cooking: "cooking",
   Art: "art",
+  Music: "music",
+  Design: "design",
+  Book: "book",
   Food: "food",
-  Event: "event",
-  Diary: "diary",
-  ThinkingFragments: "thinking-fragments",
-  Sento: "sento",
   Link: "link",
-  ProductDev: "product-dev",
-  Sauna: "sauna",
-  Items: "items",
-  Notion: "notion",
+  News: "news",
+  Tech: "tech",
 } as const;
 
 export type TagSlug = ValueOf<typeof TAG_SLUGS>;
@@ -30,90 +23,50 @@ type TagData = {
 };
 
 const TAG_DATA: Record<TagSlug, TagData> = {
-  [TAG_SLUGS.Tech]: {
-    name: "技術",
-    emoji: "💻",
-    slug: TAG_SLUGS.Tech,
+  [TAG_SLUGS.News]: {
+    name: "News",
+    emoji: "📰",
+    slug: TAG_SLUGS.News,
   },
-  [TAG_SLUGS.Update]: {
-    name: "アップデート",
-    emoji: "📝",
-    slug: TAG_SLUGS.Update,
+  [TAG_SLUGS.Cooking]: {
+    name: "Cooking",
+    emoji: "🍳",
+    slug: TAG_SLUGS.Cooking,
   },
   [TAG_SLUGS.Music]: {
-    name: "音楽",
+    name: "Music",
     emoji: "🎵",
     slug: TAG_SLUGS.Music,
   },
-  [TAG_SLUGS.Playlist]: {
-    name: "プレイリスト",
-    emoji: "🎧",
-    slug: TAG_SLUGS.Playlist,
-  },
-  [TAG_SLUGS.ThinkingFragments]: {
-    name: "思考の断片",
-    emoji: "💭",
-    slug: TAG_SLUGS.ThinkingFragments,
-  },
-  [TAG_SLUGS.Sento]: {
-    name: "銭湯",
-    emoji: "♨️",
-    slug: TAG_SLUGS.Sento,
-  },
-  [TAG_SLUGS.Sauna]: {
-    name: "サウナ",
-    emoji: "🧖‍♂️",
-    slug: TAG_SLUGS.Sauna,
+  [TAG_SLUGS.Design]: {
+    name: "Design",
+    emoji: "🎨",
+    slug: TAG_SLUGS.Design,
   },
   [TAG_SLUGS.Link]: {
-    name: "外部リンク",
+    name: "Link",
     emoji: "🔗",
     slug: TAG_SLUGS.Link,
   },
-  [TAG_SLUGS.ProductDev]: {
-    name: "プロダクト開発",
-    emoji: "🛠",
-    slug: TAG_SLUGS.ProductDev,
-  },
-  [TAG_SLUGS.Items]: {
-    name: "アイテム",
-    emoji: "✨",
-    slug: TAG_SLUGS.Items,
-  },
-  [TAG_SLUGS.Notion]: {
-    name: "Notion",
-    emoji: "📓",
-    slug: TAG_SLUGS.Notion,
-  },
-  [TAG_SLUGS.CultureBook]: {
-    name: "カルチャーブック",
-    emoji: "📖",
-    slug: TAG_SLUGS.CultureBook,
-  },
   [TAG_SLUGS.Book]: {
-    name: "本",
+    name: "Book",
     emoji: "📚",
     slug: TAG_SLUGS.Book,
   },
   [TAG_SLUGS.Art]: {
-    name: "美術・芸術",
-    emoji: "🖼️",
+    name: "Art",
+    emoji: "🖼",
     slug: TAG_SLUGS.Art,
   },
   [TAG_SLUGS.Food]: {
-    name: "食",
+    name: "Food",
     emoji: "🍙",
     slug: TAG_SLUGS.Food,
   },
-  [TAG_SLUGS.Event]: {
-    name: "イベント",
-    emoji: "🎪",
-    slug: TAG_SLUGS.Event,
-  },
-  [TAG_SLUGS.Diary]: {
-    name: "近況報告",
-    emoji: "📢",
-    slug: TAG_SLUGS.Diary,
+  [TAG_SLUGS.Tech]: {
+    name: "Tech",
+    emoji: "💻",
+    slug: TAG_SLUGS.Tech,
   },
   [TAG_SLUGS.All]: {
     name: "All",
