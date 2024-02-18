37838dd4b5d7c7dfeb49909e559d82f78e235d54
yokinist <ikd618@gmail.com>
2024-02-19 00:00:24 +0900

    refactor: NotionComponents

--- components/Notion/NotionRenderer.tsx
+++ components/Notion/NotionRenderer.tsx
@@ -1,51 +1,6 @@
 import { useTheme } from "next-themes";
-import dynamic from "next/dynamic";
 import { NotionRenderer as Renderer } from "react-notion-x";
-
-// Lazy-load some heavy components & override the renderers of some block types
-const components = {
-  /* Lazy-load */
-
-  // Code block
-  Code: dynamic(() => {
-    return import("react-notion-x/build/third-party/code").then(
-      async (module) => {
-        // #TODO: apply language styles with prismjs
-        return module.Code;
-      },
-    );
-  }),
-  // Database block
-  Collection: dynamic(() => {
-    return import("react-notion-x/build/third-party/collection").then(
-      (module) => module.Collection,
-    );
-  }),
-  // Equation block & inline variant
-  Equation: dynamic(() => {
-    return import("react-notion-x/build/third-party/equation").then(
-      (module) => module.Equation,
-    );
-  }),
-  // PDF (Embed block)
-  Pdf: dynamic(
-    () => {
-      return import("react-notion-x/build/third-party/pdf").then(
-        (module) => module.Pdf,
-      );
-    },
-    { ssr: false },
-  ),
-  // Tweet block
-  Tweet: dynamic(() => {
-    return import("react-tweet-embed").then((module) => {
-      const { default: TweetEmbed } = module;
-      return function Tweet({ id }: { id: string }) {
-        return <TweetEmbed tweetId={id} options={{ theme: "dark" }} />;
-      };
-    });
-  }),
-};
+import { NotionComponents } from "~/components/Notion/NotionComponents";
 
 const mapPageUrl = (id: string) =>
   `https://www.notion.so/${id.replace(/-/g, "")}`;
@@ -58,7 +13,7 @@ export const NotionRenderer = (props: Props) => {
   const { theme } = useTheme();
   return (
     <Renderer
-      components={components}
+      components={NotionComponents}
       mapPageUrl={mapPageUrl}
       {...props}
       darkMode={theme !== "light"}
