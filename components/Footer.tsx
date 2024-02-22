8cdaff96296393365812fe1735ff0d5b6c1f54e8
yokinist <ikd618@gmail.com>
2024-02-22 23:43:41 +0900

    refactor: Simplify footer copyright display

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,9 +5,6 @@ import Vercel from "~/components/Vercel";
 type Props = { fullWidth?: boolean };
 
 export const Footer: React.VFC<Props> = ({ fullWidth }) => {
-  const d = new Date();
-  const y = d.getFullYear();
-  const from = +BLOG.since;
   return (
     <div
       className={classNames(
@@ -21,10 +18,7 @@ export const Footer: React.VFC<Props> = ({ fullWidth }) => {
       <hr className="border-gray-200 dark:border-gray-600" />
       <div className="my-4 text-sm leading-6">
         <div className="flex flex-wrap justify-between align-baseline">
-          <p>
-            © {BLOG.author} {from === y || !from ? y : `${from} - ${y}`} - Build
-            with Next.js & Notion
-          </p>
+          <p>© {BLOG.author}</p>
           <Vercel />
         </div>
       </div>
